4afeb046e83cca6a850ea6042ad362388a2864df
Sheralieva Aizhana <asheralieva7@gmail.com>
2024-12-24 10:00:55 +0600

    Created button get 5 cards and gave it method iNeedToReceiveFiveCards. With the help of setMyHand from the state and the getCards method i can get 5 cards from the deck. Level 3 is completed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
 import './App.css';
 import CardObject from "./components/CardObject/CardObject.tsx";
+import {useState} from "react";
+import CardDeck from "./lib/CardDeck.ts";
+import Card from "./lib/Card.ts";
 
-function App() {
+const cardDeck = new CardDeck();
 
+const App = () => {
+    const [myHand, setMyHand] = useState<Card[]>([]);
+
+    const iNeedToReceiveFiveCards = () => {
+        setMyHand(cardDeck.getCards(5));
+    };
 
     return (
         <>
-            <CardObject rank='K' suit='hearts' />
+            <h3>There are cards: {cardDeck.deck.length}</h3>
+            {cardDeck.deck.length < 5 ? null
+                :
+                <button onClick={iNeedToReceiveFiveCards}>Deal cards</button>
+            }
+
+            <div className="playingCards faceImages">
+                {myHand.map((card) => (
+                    <CardObject key={card.rank + card.suit} rank={card.rank} suit={card.suit}/>
+                ))}
+            </div>
         </>
     );
-}
+};
 
 export default App;
